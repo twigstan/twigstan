9aa2d469ec81c1d9eb2d83761d09ebb82d831671
Ruud Kamphuis <ruudk@users.noreply.github.com>
2024-09-06 16:31:29 +0200

    Remove old methods from stub

--- stubs/functions.php
+++ stubs/functions.php
@@ -4,18 +4,8 @@ function twigstan_type_hint(string $type) {}
 
 function twigstan_extends(string $template, array $context): void {}
 
-function twigstan_has_block(string $bock): bool {}
-
 /**
  * @param array<mixed> $context
  * @param array<mixed> $variables
  */
 function twigstan_include(string $template, array $context, array $variables, bool $only): void {}
-
-/**
- * @template T of object
- * @param class-string<T> $class
- *
- * @return T
- */
-function twigstan_get_object(string $class): object {}
