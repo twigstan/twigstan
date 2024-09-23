89ab21d559352e5ad8266b83e39399021c3b36d3
Ruud Kamphuis <ruudk@users.noreply.github.com>
2024-09-23 12:48:51 +0200

    Add test for optionality of context variables

---
+++ tests/EndToEnd/RenderPoints/OptionalityController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EndToEnd\RenderPoints;
+
+use Twig\Environment;
+
+final class OptionalityController
+{
+    public function firstAction(Environment $environment, ?string $name): string
+    {
+        return $environment->render('EndToEnd/RenderPoints/optionality.html.twig', [
+            'name' => $name,
+        ]);
+    }
+
+    public function secondAction(Environment $environment): string
+    {
+        return $environment->render('EndToEnd/RenderPoints/optionality.html.twig');
+    }
+}
