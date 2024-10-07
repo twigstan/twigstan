b65a07b89b56cb8f89114c76599597f56452ff37
Ruud Kamphuis <ruudk@users.noreply.github.com>
2024-10-07 10:35:23 +0200

    Use separate phpstan.neon for tests

--- tests/twigstan.php
+++ tests/twigstan.php
@@ -6,5 +6,5 @@ use TwigStan\Config\TwigStanConfig;
 
 return TwigStanConfig::configure()
     ->tempDirectory(__DIR__ . '/.twigstan')
-    ->phpstanConfigurationFile(__DIR__ . '/../phpstan.neon')
+    ->phpstanConfigurationFile(__DIR__ . '/phpstan.neon')
     ->twigEnvironmentLoader(__DIR__ . '/twig-loader.php');
