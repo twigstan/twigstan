31c73e2e187f8998c0c659c5fce0d88dc9fee41d
Ruud Kamphuis <ruudk@users.noreply.github.com>
2024-10-17 16:11:16 +0200

    Remove nullability from twigstan PHPStan configuration

--- src/PHPStan/AnalysisResultToJson.php
+++ src/PHPStan/AnalysisResultToJson.php
@@ -6,24 +6,17 @@ namespace TwigStan\PHPStan;
 
 use PHPStan\Command\AnalysisResult;
 use PHPStan\Command\ErrorFormatter\ErrorFormatter;
-use PHPStan\Command\ErrorFormatter\JsonErrorFormatter;
 use PHPStan\Command\Output;
 use TwigStan\PHPStan\Collector\ExportingCollector;
 
 final readonly class AnalysisResultToJson implements ErrorFormatter
 {
     public function __construct(
-        private JsonErrorFormatter $jsonErrorFormatter,
-        private ?string $jsonFile = null,
+        private string $jsonFile,
     ) {}
 
     public function formatErrors(AnalysisResult $analysisResult, Output $output): int
     {
-        if ( ! isset($this->jsonFile)) {
-            // @phpstan-ignore phpstanApi.method (we don't care about this)
-            return $this->jsonErrorFormatter->formatErrors($analysisResult, $output);
-        }
-
         file_put_contents(
             $this->jsonFile,
             json_encode([
