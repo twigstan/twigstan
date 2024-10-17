785311fb381e75803b86e012bfd0eb9782e18527
Ruud Kamphuis <ruudk@users.noreply.github.com>
2024-10-17 14:40:20 +0200

    Update default configuration

--- example/twigstan.php
+++ example/twigstan.php
@@ -2,26 +2,23 @@
 
 declare(strict_types=1);
 
-use TwigStan\Config\TwigStanConfig;
-use TwigStan\Error\IgnoreError;
-
-return TwigStanConfig::configure()
+return TwigStan\Config\TwigStanConfig::configure()
     // ->baselineFile(__DIR__ . '/twigstan-baseline.php')
     ->reportUnmatchedIgnoredErrors(true)
     ->phpstanConfigurationFile(__DIR__ . '/phpstan.neon')
     ->phpstanMemoryLimit(false)
-    ->twigEnvironmentLoader(__DIR__ . '/twig-loader.untrack.php')
+    ->twigEnvironmentLoader(__DIR__ . '/twig-loader.php')
     ->twigPaths(__DIR__ . '/tests/EndToEnd/Types')
-    ->twigExcludes('something.html.twig')
+    // ->twigExcludes('something.html.twig')
     ->phpPaths(__DIR__ . '/tests/Fixtures')
-    ->phpExcludes('something.php')
-    ->ignoreErrors(
-        IgnoreError::create('#SomeOther#', 'someIdentifier.someValue', __DIR__ . '/some/path.php'),
-        IgnoreError::message('#SomePattern#'),
-        IgnoreError::identifier('someIdentifier'),
-        IgnoreError::path(__DIR__ . '/some/path.php'),
-        IgnoreError::messageAndIdentifier('#SomePattern#', 'someIdentifier'),
-        IgnoreError::messageAndPath('#SomePattern#', __DIR__ . '/some/path.php'),
-        IgnoreError::identifierAndPath('someIdentifier', __DIR__ . '/some/path.php'),
-    )
+    // ->phpExcludes('something.php')
+    // ->ignoreErrors(
+    //     TwigStan\Error\IgnoreError::create('#SomeOther#', 'someIdentifier.someValue', __DIR__ . '/some/path.php'),
+    //     TwigStan\Error\IgnoreError::message('#SomePattern#'),
+    //     TwigStan\Error\IgnoreError::identifier('someIdentifier'),
+    //     TwigStan\Error\IgnoreError::path(__DIR__ . '/some/path.php'),
+    //     TwigStan\Error\IgnoreError::messageAndIdentifier('#SomePattern#', 'someIdentifier'),
+    //     TwigStan\Error\IgnoreError::messageAndPath('#SomePattern#', __DIR__ . '/some/path.php'),
+    //     TwigStan\Error\IgnoreError::identifierAndPath('someIdentifier', __DIR__ . '/some/path.php'),
+    // )
     ->create();
