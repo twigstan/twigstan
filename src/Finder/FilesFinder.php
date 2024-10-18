d6986e490c546b6f77be552094d143ae06b6f3eb
Ruud Kamphuis <ruudk@users.noreply.github.com>
2024-10-18 12:58:30 +0200

    Allow specifying files in twig and php paths

--- src/Finder/FilesFinder.php
+++ src/Finder/FilesFinder.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace TwigStan\Finder;
 
+use InvalidArgumentException;
+use Symfony\Component\Filesystem\Path;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\SplFileInfo;
 
@@ -17,6 +19,7 @@ final class FilesFinder
         private string $namePattern,
         private array $paths,
         private array $exclusions,
+        private string $currentWorkingDirectory,
     ) {}
 
     /**
@@ -28,10 +31,35 @@ final class FilesFinder
             return [];
         }
 
+        $directories = [];
+        $files = [];
+        foreach ($this->paths as $path) {
+            if (is_dir($path)) {
+                $directories[] = $path;
+                continue;
+            }
+
+            if (is_file($path)) {
+                $files[] = new SplFileInfo(
+                    $path,
+                    basename(Path::makeRelative($path, $this->currentWorkingDirectory)),
+                    Path::makeRelative($path, $this->currentWorkingDirectory),
+                );
+                continue;
+            }
+
+            throw new InvalidArgumentException(sprintf('Path %s is not a file or directory', $path));
+        }
+
+        if ($files === [] && $directories === []) {
+            return [];
+        }
+
         $finder = Finder::create()
             ->files()
             ->name([$this->namePattern])
-            ->in($this->paths)
+            ->in($directories)
+            ->append($files)
             ->sortByName()
             ->filter(function (SplFileInfo $file) {
                 foreach ($this->exclusions as $exclude) {
