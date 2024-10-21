135e91694cc6431a6e8ddbbf52849c88aeaaec35
Ruud Kamphuis <ruudk@users.noreply.github.com>
2024-10-21 12:19:15 +0200

    Fetch `twig` from `test.service_container`
    Fixes #16

--- example/twig-loader-symfony.php
+++ example/twig-loader-symfony.php
@@ -9,20 +9,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 
-$kernel = new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+$kernel = new Kernel('test', true);
 $kernel->boot();
 
-// echo "Make sure that the `twig` service is public in your Symfony application
-// by adding a compiler pass to your Kernel:
-//
-// protected function build(\Symfony\Component\DependencyInjection\ContainerBuilder $container) : void
-// {
-//     $container->addCompilerPass(new class implements \Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface {
-//         public function process(\Symfony\Component\DependencyInjection\ContainerBuilder $container) : void
-//         {
-//             $container->getDefinition('twig')->setPublic(true);
-//         }
-//     });
-// }
-
-return $kernel->getContainer()->get('twig');
+return $kernel->getContainer()->get('test.service_container')->get('twig');
