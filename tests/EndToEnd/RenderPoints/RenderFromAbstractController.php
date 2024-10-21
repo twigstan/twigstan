4eeeee4c0577ff71b4e498b27a05ff75d0f4d007
Ruud Kamphuis <ruudk@users.noreply.github.com>
2024-10-21 11:58:14 +0200

    Automatically convert FormInterface to FormView (#69)
    * Fix namespace in tests
    
    * Automatically convert FormInterface to FormView
    
    When using the AbstractController, Symfony automatically converts a Form
    instance to a FormView.

--- tests/EndToEnd/RenderPoints/RenderFromAbstractController.php
+++ tests/EndToEnd/RenderPoints/RenderFromAbstractController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace TwigStan\EndToEnd\RenderPoints;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 
 final class RenderFromAbstractController extends AbstractController
@@ -25,5 +26,21 @@ final class RenderFromAbstractController extends AbstractController
             'title' => 'RenderViewAction',
             'artists' => ['Adele', 'Kanye West'],
         ]));
+    }
+
+    public function formAction(): Response
+    {
+        $form1 = $this->createFormBuilder()
+            ->add('name', TextType::class, [
+                'label' => 'Your Name',
+            ])
+            ->getForm();
+
+        $form2 = $this->createForm(RegisterForm::class, new RegisterFormDto());
+
+        return $this->render('EndToEnd/RenderPoints/form.html.twig', [
+            'form1' => $form1,
+            'form2' => $form2,
+        ]);
     }
 }
