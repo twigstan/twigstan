d94aca7b05596767f1065a075ad18e8737b7d149
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2024-10-23 13:48:28 +0200

    Improve support of twig context (#78)

--- tests/EndToEnd/RenderPoints/RenderFromTwig.php
+++ tests/EndToEnd/RenderPoints/RenderFromTwig.php
@@ -8,9 +8,19 @@ use Twig\Environment;
 
 final class RenderFromTwig
 {
+    public function __construct(private readonly Environment $environment) {}
+
     public function generateContent(Environment $environment): string
     {
         return $environment->render('EndToEnd/RenderPoints/render.html.twig', [
+            'title' => 'RenderAction',
+            'artists' => ['Adele', 'Kanye West'],
+        ]);
+    }
+
+    public function generateContent2(): string
+    {
+        return $this->environment->render('EndToEnd/RenderPoints/render.html.twig', [
             'title' => 'RenderAction',
             'artists' => ['Adele', 'Kanye West'],
         ]);
