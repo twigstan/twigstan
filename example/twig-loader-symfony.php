ff6b3133efd14cdd6a489336ad4be176f0066d38
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2024-10-23 12:38:01 +0200

    Update twig-loader-symfony.php (#77)

--- example/twig-loader-symfony.php
+++ example/twig-loader-symfony.php
@@ -7,7 +7,7 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require __DIR__ . '/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+(new Dotenv())->bootEnv(__DIR__ . '/.env');
 
 $kernel = new Kernel('test', true);
 $kernel->boot();
