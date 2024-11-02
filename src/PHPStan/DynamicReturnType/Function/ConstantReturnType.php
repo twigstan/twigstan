2e4b9a89cde3fa29b1fe78e4a1898b31b42a7799
Ruud Kamphuis <ruudk@users.noreply.github.com>
2024-11-02 12:13:24 +0100

    Support all encoding filters
    Limited support has been added. Some edge cases might not work (yet).

---
+++ src/PHPStan/DynamicReturnType/Function/ConstantReturnType.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TwigStan\PHPStan\DynamicReturnType\Function;
+
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\StaticCall;
+use PhpParser\Node\Name;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\DynamicStaticMethodReturnTypeExtension;
+use PHPStan\Type\Type;
+use Twig\Extension\CoreExtension;
+
+final readonly class ConstantReturnType implements DynamicStaticMethodReturnTypeExtension
+{
+    public function getClass(): string
+    {
+        return CoreExtension::class;
+    }
+
+    public function isStaticMethodSupported(MethodReflection $methodReflection): bool
+    {
+        return $methodReflection->getName() === 'constant';
+    }
+
+    public function getTypeFromStaticMethodCall(
+        MethodReflection $methodReflection,
+        StaticCall $methodCall,
+        Scope $scope,
+    ): ?Type {
+        if (count($methodCall->args) !== 1) {
+            return null;
+        }
+
+        return $scope->getType(new FuncCall(new Name('constant'), $methodCall->args));
+    }
+}
