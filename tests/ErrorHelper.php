07230822c45add40b653a8cab2e8a871fd72c05f
Ruud Kamphuis <ruudk@users.noreply.github.com>
2024-11-27 19:32:45 +0100

    Make errors.json file optional
    And validate if it's the correct structure.

--- tests/ErrorHelper.php
+++ tests/ErrorHelper.php
@@ -24,12 +24,23 @@ final readonly class ErrorHelper
     public static function assertAnalysisResultMatchesJsonFile(TwigStanAnalysisResult $result, string $directory, array $files = []): void
     {
         $filesystem = new Filesystem();
-        $expectedErrors = json_decode(
-            $filesystem->readFile(Path::join($directory, 'errors.json')),
-            true,
-            512,
-            JSON_THROW_ON_ERROR,
-        );
+        $expectedErrors = [
+            'errors' => [],
+            'fileSpecificErrors' => [],
+        ];
+
+        if (file_exists(Path::join($directory, 'errors.json'))) {
+            $expectedErrors = json_decode(
+                $filesystem->readFile(Path::join($directory, 'errors.json')),
+                true,
+                512,
+                JSON_THROW_ON_ERROR,
+            );
+
+            Assert::assertIsArray($expectedErrors);
+            Assert::assertArrayHasKey('errors', $expectedErrors);
+            Assert::assertArrayHasKey('fileSpecificErrors', $expectedErrors);
+        }
 
         $versionSpecificErrorsFile = Path::join($directory, sprintf('errors.v%d.json', Environment::MAJOR_VERSION));
 
